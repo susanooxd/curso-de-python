6339498f7becef6fbe5e748b704dfcb4af2b4ceb
Jorge <86664565+susanooxd@users.noreply.github.com>
2021-10-07 12:23:27 -0300

    Add files via upload

---
+++ operadores artméticos.py
@@ -0,0 +1,26 @@
+# OPERADORES ARTMÉTICOS
+# Todos os operadores precisam de um operando, números ou variaveis strings
+
+
+# + Adição
+# - Subtração
+# * Multiplicação
+# / Divisão
+# ** Potência
+# // Divisão Inteira
+# % Resto da Divisão
+
+# 5 + 2 == 7
+# 5 - 2 == 3
+# 5 * 2 == 10
+# 5 / 2 == 2.5
+# 5 ** 2 == 25
+# 5 // 2 == 2
+# 5 % 2 == 1
+
+# Ordem de Precedência
+# 1 () parenteses
+# 2 ** potência
+# 2 * / // %
+# 3 + - 
+# 4
